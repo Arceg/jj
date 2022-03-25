5394d3dad89ba982e7e5eba8759113654addf01d
ŁukaszCapała <Arceg001@hotmail.com>
2022-03-25 12:56:37 +0100

    Initial commit

---
+++ ok.java
@@ -0,0 +1,11 @@
+public static void main(String[]args){
+
+Scanner sc = new Scanner(system.in);
+System.out.printIn("podaj liczbę")
+        int number = sc nextInt();
+if (number % 3 == 0)
+    System.out.printIn("liczba jest podzielna przez 3")
+}
+else{
+    System.out.printIn("liczba nie jest podzielna przez 3")
+        }
---
+++ xd/src/zadanie1.java
@@ -0,0 +1,11 @@
+public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printIn("Podaj Imię")
+        String name = sc.next();
+        if(startswith= "J" )
+        System.out.printIn("Twoje imie zaczyna sie na litere J");
+        }
+        else{
+        System.out.printIn("Twoje imie nie zaczyna sie na litere J");
+        }
