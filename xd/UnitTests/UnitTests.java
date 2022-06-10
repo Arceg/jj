a2578f1fb03d501ee1c5201e490769dea22efbe4
Arceg <101935035+Arceg@users.noreply.github.com>
2022-06-10 20:43:53 +0200

    Add files via upload

---
+++ xd/UnitTests/UnitTests.java
@@ -0,0 +1,72 @@
+public class UnitTests {
+    public char wynik(int ocenaL)
+        if(ocenaL< 0)
+
+    {
+        throw new IllegalArgumentException("Błąd")
+    }
+        else if(ocenaL< 5)
+            return'Bardzo słabo'
+            }
+            else if (ocenaL < 5)
+            return 'Bardzo słabo'
+        }
+        else if (ocenaL < 10)
+        return 'dość słabo'
+        }
+        else if (ocenaL < 15)
+        return 'słabo'
+        }
+        else if (ocenaL < 20)
+        return 'słabo ale ok'
+        }
+        else if (ocenaL < 25)
+        return 'słabo ale lepiej niż ok'
+        }
+        else if (ocenaL < 30)
+        return 'prawie ok'
+        }
+        else if (ocenaL < 35)
+        return 'blisko ok'
+        }
+        else if (ocenaL < 40)
+        return 'Bardzo blisko ok'
+        }
+        else if (ocenaL < 45)
+        return 'ok'
+        }
+        else if (ocenaL < 50)
+        return 'średnio'
+        }
+        else if (ocenaL < 55)
+        return 'troche lepiej niż średnio'
+        }
+        else if (ocenaL < 60)
+        return 'Lepiej niż średnio'
+        }
+        else if (ocenaL < 65)
+        return 'Znacznie lepiej niż średnio'
+        }
+        else if (ocenaL < 70)
+        return 'Dobrze'
+        }
+        else if (ocenaL < 75)
+        return 'Lepiej niż dobrze'
+        }
+        else if (ocenaL < 80)
+        return 'Jeszcze lepiej niż dobrze'
+        }
+        else if (ocenaL < 85)
+        return 'Bardzo dobrze'
+        }
+        else if (ocenaL < 90)
+        return 'świetnie'
+        }
+        else if (ocenaL < 95)
+        return 'Wybitnie'
+        }
+        else if (ocenaL < 100)
+        return 'Wow jesteś super'
+        }
+        }
+}
---
+++ xd/UnitTests/UnitTestyx20.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitTestyx20 {
+@Test
+    void mniejniz5() {
+    var wynik = new wynik();
+    return 0,1,2,3,4,;
+}
+
+    @Test
+    void mniejniz10() {
+        var wynik = new wynik();
+        return 5,6,7,8,9,10;
+}
+@Test
+    void mniejniz15(){
+        var wynik=new wynik();
+        return 11,12,13,14,15;
+    }
+    @Test
+    void mniejniz20(){
+        var wynik=new wynik();
+        return 16,17,18,19,20;
+    }
+    @Test
+    void mniejniz25(){
+        var wynik=new wynik();
+        return 21,22,23,24,25;
+    }
+    @Test
+    void mniejniz30(){
+        var wynik=new wynik();
+        return 26,27,28,29,30;
+    }
+    @Test
+    void mniejniz35(){
+        var wynik=new wynik();
+        return 31,32,33,34,35;
+    }
+    @Test
+    void mniejniz40(){
+        var wynik=new wynik();
+        return 36,37,38,39,40;
+    }
+    @Test
+    void mniejniz45(){
+        var wynik=new wynik();
+        return 41,42,43,44,45;
+    }
+    @Test
+    void mniejniz50(){
+        var wynik=new wynik();
+        return 46,47,48,49,50;
+    }
+    @Test
+    void mniejniz55(){
+        var wynik=new wynik();
+        return 51,52,53,54,55;
+    }
+    @Test
+    void mniejniz60(){
+        var wynik=new wynik();
+        return 56,57,58,59,60;
+    }
+    @Test
+    void mniejniz65(){
+        var wynik=new wynik();
+        return 61,62,63,64,65;
+    }
+    @Test
+    void mniejniz70(){
+        var wynik=new wynik();
+        return 66,67,68,69,70;
+    }
+    @Test
+    void mniejniz75(){
+        var wynik=new wynik();
+        return 71,72,73,74,75;
+    }
+    @Test
+    void mniejniz80(){
+        var wynik=new wynik();
+        return 76,77,78,79,80;
+    }
+    @Test
+    void mniejniz85(){
+        var wynik=new wynik();
+        return 81,82,83,84,85;
+    }
+    @Test
+    void mniejniz90(){
+        var wynik=new wynik();
+        return 86,87,88,89,90;
+    }
+    @Test
+    void mniejniz95(){
+        var wynik=new wynik();
+        return 91,92,93,94,95;
+    }
+    @Test
+    void mniejniz100(){
+        var wynik=new wynik();
+        return 96,97,98,99,100;
+    }
+
